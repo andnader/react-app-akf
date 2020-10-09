b9db8a8279e98b953e50561b1d0e206feaa8813d
Nader <andnader@a483e795b4a2.ant.amazon.com>
2020-10-08 20:35:39 -0400

    passing state reactjs2

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -9,43 +9,12 @@ const useStyles = makeStyles((theme) => ({
     },
   },
 }));
-/*
-export default function BasicTextFields() {
-  const classes = useStyles();
-  const searchContext = React.createContext("");
-  const [value, setValue] = useState("");
-  const handleChange = e => {
-    console.log(`Typed => ${e.target.value}`);
-    setValue(e.target.value)
-  }
-  return (
-    <form className={classes.root} noValidate autoComplete="on">
-      <Input placeholder="Search" value={value} onChange={handleChange} inputProps={{ 'aria-label': 'description' }} type="sumbit" id="Go" name="searchinput"/>
-    </form>
-  );
-}
-
-export default function BasicTextFields(props) {
-  const classes = useStyles();
-  const [searchValue, setValue] = useState("");
-
-  function changeSearch(e) {
-    setValue(e.target.value)
-  }
-
-  return (
-    <form className={classes.root} noValidate autoComplete="on">
-      <Input placeholder="Search" onChangeCapture={changeSearch} value={searchValue} inputProps={{ 'aria-label': 'description' }} type="text" />
-    </form>
-  );
-}*/
 
 export default function BasicTextFields(props) {
   const classes = useStyles();
   const [search, setValue] = useState("")
 
   function changeSearch(e) {
-    
     setValue(e.target.value)
   }
 
